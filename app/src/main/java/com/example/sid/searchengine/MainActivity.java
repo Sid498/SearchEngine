8108c8f22d1c5640cf15a7e9e3004888f8bb635a
Sid498 <sidpawar619@gmail.com>
2017-06-18 19:18:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sid/searchengine/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sid.searchengine;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Search(View v){
+        WebView myweb = (WebView) findViewById(R.id.Browesr);
+
+        myweb.setWebViewClient(new WebViewClient());
+
+        EditText text = (EditText) findViewById(R.id.etSearchhere);
+        String url = "https://www.google.com/search?q="+text.getText().toString();
+        myweb.loadUrl(url);
+    }
+}
